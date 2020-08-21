78a60073312a42c5e8341a3e33a284191949229a
T70GR3S <69549097+T70GR3S@users.noreply.github.com>
2020-08-21 23:33:15 +0300

    Add files via upload

---
+++ hıztesti.py
@@ -0,0 +1,38 @@
+import requests
+import re
+import time
+import random
+from colorama import Fore, Back, Style 
+
+
+def get(url):
+    response = requests.get(url)
+    expression = """<td height="20">([^<]+)</td>
+<td>([^<]+)</td>"""
+
+    if response.status_code == 200:
+        a = re.findall(expression, response.text)
+        return [__ for _ in a for __ in _]
+
+
+print(Fore.CYAN+"-----Çıkmak İçin Q ye basınız----")
+print(Style.RESET_ALL)
+ilk_zaman = time.time()
+while True:
+
+    start = time.time()
+    isimler = random.choice(get('https://huseyindemirtas.net/kullanici-adi-listesi-ad-soyad-listesi/'))
+    print(Fore.RED+isimler)
+    print(Style.RESET_ALL)
+
+    user = input(Fore.LIGHTMAGENTA_EX+"Kelimeyi Gir: ")
+
+    print(time.time() - start)
+    if user == isimler:
+        print("Doğru")
+    elif user == "Q":
+        break
+    else:
+        print('yanlış')
+
+print("Çıkış Yapılıyor\nToplam Geçen Zaman =", time.time() - ilk_zaman)
